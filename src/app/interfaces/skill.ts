b549f2c3b017bc3049bae53f9c5270ab36922d7a
GusthavoSoares <gusthavorsoares@gmail.com>
2024-08-25 21:35:08 -0300

    page-update: skills media-queries (08/25/2024)

--- src/app/interfaces/skill.ts
+++ src/app/interfaces/skill.ts
@@ -1,5 +1,5 @@
 export interface Skill {
+  id: string,
   name: string,
-  description: string,
   icon: string
 }
--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../../components/menu/menu.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { Skill } from '../../interfaces/skill';
+import { CardComponent } from "../../components/card/card.component";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [MenuComponent, FooterComponent],
+  imports: [MenuComponent, FooterComponent, CardComponent, ReactiveFormsModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-  
+  skills: Skill[] = [
+    { id: uuidv4(), name: "Angular", icon: "fa-angular" },
+    { id: uuidv4(), name: "CSS", icon: "fa-css3" },
+    { id: uuidv4(), name: "HTML", icon: "fa-html5" },
+    { id: uuidv4(), name: "Python", icon: "fa-python" }
+  ]
+
+
+
 }
