752f57920741c6bc5ec8d7453a3c4feabf285890
GusthavoSoares <gusthavorsoares@gmail.com>
2024-09-07 20:12:15 -0300

    style (9/7/24): Making the grid100 and grid50-50 dynamically with a class property at class component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -3,13 +3,14 @@ import { MenuComponent } from '../../components/menu/menu.component';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { CardComponent } from '../../components/card/card.component';
 import { ImageComponent } from "../../components/image/image.component";
+import { ContainerComponent } from '../../components/container/container.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [MenuComponent,
     FooterComponent,
-    CardComponent, ImageComponent],
+    CardComponent, ImageComponent, ContainerComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
