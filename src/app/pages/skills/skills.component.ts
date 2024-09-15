508cda5e9cc8ef4c6c948f36e4d8e726798e9a3c
GusthavoSoares <gusthavorsoares@gmail.com>
2024-09-15 18:56:09 -0300

    feature (9/15/24): contact form project type selection

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -5,6 +5,7 @@ import { Skill } from '../../interfaces/skill';
 import { CardComponent } from "../../components/card/card.component";
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { v4 as uuidv4 } from 'uuid';
+import { debounceTime, distinctUntilChanged, map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-skills',
@@ -20,7 +21,18 @@ export class SkillsComponent {
     { id: uuidv4(), name: "HTML", icon: "fa-html5" },
     { id: uuidv4(), name: "Python", icon: "fa-python" }
   ]
+  searchControl = new FormControl('');
+  filteredSkills$: Observable<Skill[]> = this.searchControl.valueChanges.pipe(
+    startWith(''),
+    debounceTime(300),
+    distinctUntilChanged(),
+    map(value => this._filterSkills(value))
+  );
 
+  private _filterSkills(value: string | null): Skill[] {
+    const filterValue = (value || '').toLowerCase();
+    return this.skills.filter(skill => skill.name.toLowerCase().includes(filterValue));
+  }
 
 
 }
